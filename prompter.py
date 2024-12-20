89dbadae506f5dadfd2b8f4efc446b66ea1dfd3a
Ronald McCollam <mccollam@gmail.com>
2024-12-20 15:28:06 -0500

    cleanup, fixes for real environment

--- prompter.py
+++ prompter.py
@@ -2,12 +2,19 @@
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 import configparser
+import logging
 import pygame
 import sys
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename='prompter.log', encoding='utf-8', level=logging.DEBUG)
+
+
 
 class PrompterUpdate(FileSystemEventHandler):
     def on_modified(self, event):
         prompter.UpdateConfig()
+
 class Prompter:
     config_file = "./prompter.ini"
 
@@ -19,8 +26,10 @@ class Prompter:
     default_font_size = 80
     default_scroll_speed = 4
     default_script = "scripts/script1.txt"
+    default_padding = 60
 
     def UpdateConfig(self):
+        logger.debug("Config file updated")
         prev_script = self.script
         self.ReadConfig()
         if prev_script != self.script:
@@ -28,32 +37,47 @@ class Prompter:
             self.LoadScript()
 
     def ReadConfig(self):
-        config = configparser.ConfigParser()
-        with open(self.config_file,"r") as conffile:
+        logger.debug("Loading configuration...")
+        try:
+            config = configparser.ConfigParser()
+            conffile = open(self.config_file, "r")
+            logger.debug("Loading config file " + self.config_file)
             config.read_file(conffile)
             self.screen_width = config.getint("display", "width", fallback=self.default_screen_width)
             self.screen_height = config.getint("display", "height", fallback=self.default_screen_height)
             self.background_color = config.get("display", "bg", fallback=self.default_bg_color)
             self.text_color = config.get("display", "text", fallback=self.default_text_color)
+            self.padding = config.get("display", "padding", fallback=self.default_padding)
             self.font_size = config.getint("font", "font_size", fallback=self.default_font_size)
             self.scroll_speed = config.getint("font", "scroll_speed", fallback=self.default_scroll_speed)
             self.script = config.get("script", "current_script", fallback=self.default_script)
+        except Exception as e:
+            logger.error(f"Error loading configuration: {str(e)}")
+        finally:
+            conffile.close()
+        # TODO: Check for invalid values, set to defaults
+
     
     def LoadScript(self):
+        logger.info("Loading script " + self.script)
         self.lines = []
         scriptfile = open(self.script, 'r')
         raw_lines = scriptfile.readlines()
         
         # Wrap each line of text
         for raw_line in raw_lines:
-            wrapped_lines = self.WrapText(raw_line.strip(), self.font, self.screen_width - 100)  # Leave some padding
+            wrapped_lines = self.WrapText(raw_line.strip(), self.font, self.screen_width - 2*self.padding)  # leave space for padding on _both_ sides
             self.lines.extend(wrapped_lines)
+        
+        # Flip the order (because we're showing upside down on the reflection)
+        self.lines.reverse()
 
         # Remember how long this text is
         self.script_height = self.font.get_height() * len(self.lines)
         
         # Start in the middle of the screen
-        self.yPosition = self.screen_height/2
+        # (measured from the "bottom" of the script because it's flipped in the reflection)
+        self.yPosition = -self.script_height + self.screen_height/2
     
     def WrapText(self, text, font, max_width):
         words = text.split(' ')
@@ -78,12 +102,13 @@ class Prompter:
     def RenderText(self, y_offset):
         for line in self.lines:
             text_surface = self.font.render(line.strip(), True, pygame.Color(self.text_color))
-            text_surface = pygame.transform.flip(text_surface, True, False)
-            text_x = self.screen_width - text_surface.get_width()
+            text_surface = pygame.transform.flip(text_surface, False, True)
+            text_x = self.padding
             self.screen.blit(text_surface, (text_x, y_offset))
             y_offset += text_surface.get_height()
 
     def __init__(self):
+        logger.info("Prompter started!")
         # Get our config data
         self.ReadConfig()
 
@@ -95,24 +120,34 @@ class Prompter:
         self.clock = pygame.time.Clock()
 
         # Watch the config file for changes
+        logger.debug("Starting watchdog observer")
         self.observer = Observer()
-    
-    def run(self):
-        print("Prompter running!")
 
         # Watch for config file updates
         update_event_handler = PrompterUpdate()
         self.observer.schedule(update_event_handler, path=self.config_file)
         self.observer.start()
+        logger.debug("Watchdog observer running")
         
+    
+    def run(self):
+        logger.info("Prompter running!")
+
         # Load the script to start
         self.LoadScript()
-
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    logger.info("Exiting...")
+                    logger.debug("Cleanup: Shutting down watchdog observer")
+                    try:
+                        self.observer.stop()
+                        self.observer.join()
+                    except Exception as e:
+                        logger.error(f"Unable to shutdown observer: {str(e)}")
                     pygame.quit()
+                    self.debug("Cleanup complete")
                     sys.exit()
 
             keys = pygame.key.get_pressed()
@@ -120,7 +155,7 @@ class Prompter:
                 if self.yPosition + self.script_height > self.font.get_height(): # Don't scroll the last line off screen
                     self.yPosition -= self.scroll_speed  # Scroll down
             if keys[pygame.K_UP]:
-                if self.yPosition < self.screen_height - self.font.get_height(): # Don't scroll the first line off screen
+                if self.yPosition < self.screen_height - 2*self.font.get_height(): # Don't scroll the first line off screen
                     self.yPosition += self.scroll_speed  # Scroll up for reversing effect
 
             # Clear screen
@@ -134,10 +169,6 @@ class Prompter:
 
             # Cap the frame rate
             self.clock.tick(60)
-
-
-        # Clean up the observer
-        self.observer.join()
 
 
 
